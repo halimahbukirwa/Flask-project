8aa325352b8f383ec0d3d2c3a994a6d93682647a
Halimah Bukirwa <halimahbukirwa24@gmail.com>
2021-04-22 16:42:02 +0300

    Modified the delete method

--- movie.py
+++ movie.py
@@ -22,49 +22,50 @@ Movies = [{'Title':"Sonic The Hedgehog",
 def index():
     return "Welcome To My Movie Library"
 
- @app.route("/Movies",methods=['GET'])
- def get_movie():
-     return jsonify({'Movies':Movies})
+@app.route("/Movies",methods=['GET'])
+def get_movie():
+    return jsonify({'Movies':Movies})
 
- @app.route("/Movies/<int:Movie_id>",methods=['GET'])
- def get_movieId(Movie_id):
-     return jsonify({'Movies':Movies[Movie_id]})
+@app.route("/Movies/<int:Movie_id>",methods=['GET'])
+def get_movieId(Movie_id):
+    return jsonify({'Movies':Movies[Movie_id]})
 
- @app.route("/Movies/<string:Title>",methods=['GET'])
- def get_movieTitle(Title):
-     return jsonify({'Movies':Movies[Title]})
+@app.route("/Movies/<string:Title>",methods=['GET'])
+def get_movieTitle(Title):
+    return jsonify({'Movies':Movies[Title]})
 
- @app.route("/Movies/<string:Genre>",methods=['GET'])
- def get_movieGenre(Genre):
-     return jsonify({'Movies':Movies[Genre]})
+@app.route("/Movies/<string:Genre>",methods=['GET'])
+def get_movieGenre(Genre):
+    return jsonify({'Movies':Movies[Genre]})
 
- @app.route("/Movies/<string:Duration>",methods=['GET'])
- def get_movieDuration(Duration):
-     return jsonify({'Movies':Movies[Duration]})
+@app.route("/Movies/<string:Duration>",methods=['GET'])
+def get_movieDuration(Duration):
+    return jsonify({'Movies':Movies[Duration]})
 
- @app.route("/Movies",methods=['POST'])
- def create():
-     Movie = {'Title':"War Room", 
+@app.route("/Movies",methods=['POST'])
+def create():
+    Movie = {'Title':"War Room", 
                'Movie_id':"4",
                'Duration':"2hrs",
                'Genre':"Drama"
                }
-     Movies.append(Movie)
-     return jsonify({'Created':Movie})
+    Movies.append(Movie)
+    return jsonify({'Created':Movie})
 
- @app.route("/Movies/<string:Movie_id>",methods=['PUT'])
- def updateMovie(Movie_id):
-     Movies['Movie_id']['Genre'] = "Christian Drama"
-     return jsonify({'Movies':Movies['Movie_id']})
+@app.route("/Movies/<string:Movie_id>",methods=['PUT'])
+def updateMovie(Movie_id):
 
-
- @app.route("/Movies/<string:Movie_id>",methods=['DELETE'])
- def delete():
-     Movies.remove(Movies[Movie_id])
-     return jsonify({'result':True})
+    Movies['Movie_id']['Genre'] = "Christian Drama"
+    return jsonify({'Movies':Movies['Movie_id']})
 
 
- if __name__ == "__main__":
+@app.route("/Movies/<string:Movie_id>",methods=['DEL'])
+def delete():
+    Movies.remove(Movies[Movie_id])
+    return jsonify({'result':True})
 
-     app.run(debug=True)
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
 
